4fd56ea9ef2a00a995cb590ad7f3fd0733f14285
Bhupen Gupta <bhupengupta05@gmail.com>
2026-03-24 15:56:03 +0530

    new app

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,12 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning={true}
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col"
+        suppressHydrationWarning={true}
+      >{children}</body>
     </html>
   );
 }
